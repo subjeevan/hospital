401cd54c3334b79d4dd76dab6ecddeb3ffa5e76f
subjeevan <99754701+subjeevan@users.noreply.github.com>
2023-07-19 18:05:32 +0545

    ver2

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from registration.views import homepage,aboutus,contact
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('homepage/',homepage,name='home'),
     path('contact/',contact,name='contact'),
     path('aboutus/',aboutus,name='about'),
