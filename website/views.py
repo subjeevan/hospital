cd0a02011bb1d367aa97ead43487234e37cce051
subjeevan <99754701+subjeevan@users.noreply.github.com>
2023-07-19 08:30:57 +0545

    ver1

--- website/views.py
+++ website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage(request):
+    return render(request,'homepage.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def aboutus(request):
+    return render(request,'aboutus.html')
